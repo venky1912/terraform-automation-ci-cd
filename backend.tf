e0165f30fa51d806f35eeec99e0d7f173e19c7e7
venky <snikommuvenky115@gmail.com>
2024-08-22 22:16:39 +0100

    create vpc in dev env

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "devopstrainee-terraform-bucket"
-    key            = "terraform/${terraform.workspace}/terraform.tfstate"
-    region         = "eu-west-2"
-  }
-}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "region" {
   type        = string
   default     = "eu-west-2"
 }
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
